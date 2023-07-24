872c9f3176f035cef2910cc5344925946431753c
nadhemDev <blendplay2@gmail.com>
2023-07-24 08:55:40 +0100

    project init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Main from "./layouts/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Main = () => {
+    return (
+        <div className='container mx-auto'>
+            <Navbar />
+            <p>Main</p>
+        </div>
+    )
+}
+
+export default Main
