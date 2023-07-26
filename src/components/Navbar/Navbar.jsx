e1621466ad9dda8d09e7949936605beb6debdb72
nadhemDev <blendplay2@gmail.com>
2023-07-26 08:27:04 +0100

    tailwind color fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
     return (
-        <div className='flex justify-between p-3'>
+        <div className=' font-krona flex justify-between p-3'>
             {/* Brand */}
-            <a className="navbar__brand" href="/">Welly</a>
+            <a className="text-2xl" href="/">Welly</a>
 
-            <div className='flex'>
-                <a href="/home">Home</a>
-                <a href="/search">Search</a>
-                <a href="/about">About</a>
+            <div className='flex item-end'>
+                <a className='text-sm mr-8' href="/home">Home</a>
+                <a className='text-sm mr-8' href="/search">Search</a>
+                <a className='text-sm mr-8' href="/about">About</a>
 
 
+                {/* ICONS*/}
+                {/*TODO add color to icons */}
+                <div className="flex ">
 
-                <div className="flex">
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                     </svg>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme'); // Import the defaultTheme
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        'base': "Poppins, sans-serif",
+        'krona': "'Krona One', sans serif",
+
+      },
+      colors: {
+        dark: "#0C080B",
+        green: "#1E3329",
+        yellow: "#FFD470",
+        grey: "#B8B8BB",
+        light: "#B8B8BB",
+
+      },
+      // Extend or override default theme settings
+      screens: {
+        'xs': '360px', // Custom screen size
+        ...defaultTheme.screens, // Include the default screens
+      },
+    },
   },
   plugins: [],
-}
+};
